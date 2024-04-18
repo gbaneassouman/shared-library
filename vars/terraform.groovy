fe6d4b020650f3cc4470180afd78daeebbec0ae2
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-18 14:04:03 +0000

    replace individual sh by global

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,27 +1,29 @@
 #!/usr/bin/env groovy
-/* groovylint-disable MethodParameterTypeRequired, MethodReturnTypeRequired, NoDef, UnusedMethodParameter */
+//
+/* groovylint-disable MethodParameterTypeRequired, MethodReturnTypeRequired, NoDef, UnusedMethodParameter */ 
 
 def init(name) {
     /* groovylint-disable-next-line GStringExpressionWithinString */
-    sh "cd src/terraform/${name}"
-    sh "terraform init -input=false"
-    
+    sh """
+        cd src/terraform/${name}
+        terraform init -input=false
+    """
 }
 
 def plan(name) {
     /* groovylint-disable-next-line GStringExpressionWithinString */
-    sh '''
+    sh """
         cd src/terraform/${name}
         terraform plan - out ${name}.plan
-    '''
+    """
 }
 
 /* groovylint-disable-next-line GStringExpressionWithinString, MethodParameterTypeRequired */
 def apply(name) {
     /* groovylint-disable-next-line GStringExpressionWithinString */
-    sh '''
+    sh """
         cd src/terraform/${name}
         terraform apply ${name}.plan
-    '''
+    """
 }
 
