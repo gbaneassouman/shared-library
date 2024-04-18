ab068ffe89904b711d9d4df637f6092d2aedfafd
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-18 15:36:46 +0000

    escape dollar sign in awk cmd

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -25,14 +25,14 @@ def copyFile(dirname) {
 }
 
 def unzipDir(dirname) {
-    sh "ssh -i $TF_DIR/${dirname}/files/$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "unzip ~/app-dir.zip" "
+    sh "ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "unzip ~/app-dir.zip" "
 }
 
 def apps(dirname) {
     sh """
-        ssh -i $TF_DIR/${dirname}/files/$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "unzip ~/app-dir.zip"
-        ssh -i $TF_DIR/${dirname}/files/$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "chmod +x ~/app-dir/deploy-apps.sh"
-        ssh -i $TF_DIR/${dirname}/files/$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "cd ~/app-dir && sh deploy-apps.sh"
+        ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "unzip ~/app-dir.zip"
+        ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "chmod +x ~/app-dir/deploy-apps.sh"
+        ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "cd ~/app-dir && sh deploy-apps.sh"
     """
 }
 
