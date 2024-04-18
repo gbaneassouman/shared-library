cb8ace1e6544e67881ac23a388673f4e67e02a22
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-18 15:24:37 +0000

    escape dollar sign in awk cmd

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,38 +2,38 @@
 /* groovylint-disable LineLength, NglParseError */
 /* groovylint-disable-next-line NglParseError */
 def exportIp(dirname) {
-    sh "export instance_ip=$(awk '{print \$1}' src/terraform/${dirname}/files/infos_ec2.txt)"
+    sh "export instance_ip=\$(awk '{print \$1}' src/terraform/${dirname}/files/infos_ec2.txt)"
 }
 
 def appDirname(dirname) {
-    sh '''
+    sh """
         INFILE=$PWD/list.txt
         mkdir -p app-dir
         for LINE in $(cat "$INFILE")
         do
             cp -r src/"$LINE" app-dir/
         done
-    '''
+    """
 }
 
 def copyFile(dirname) {
-    sh '''
+    sh """
         cp src/scripts/deploy-apps.sh app-dir/ && cp src/terraform/${dirname}/files/infos_ec2.txt app-dir/
         zip -r app-dir.zip app-dir/
         scp -i $TF_DIR/${dirname}/files/$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no -r app-dir.zip $username@$instance_ip:~/
-    '''
+    """
 }
 
 def unzipDir(dirname) {
-    sh ' ssh -i $TF_DIR/${dirname}/files/$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "unzip ~/app-dir.zip" '
+    sh "ssh -i $TF_DIR/${dirname}/files/$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "unzip ~/app-dir.zip" "
 }
 
 def apps(dirname) {
-    sh '''
+    sh """
         ssh -i $TF_DIR/${dirname}/files/$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "unzip ~/app-dir.zip"
         ssh -i $TF_DIR/${dirname}/files/$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "chmod +x ~/app-dir/deploy-apps.sh"
         ssh -i $TF_DIR/${dirname}/files/$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip "cd ~/app-dir && sh deploy-apps.sh"
-    '''
+    """
 }
 
 def deleteDirs(dirname) {
