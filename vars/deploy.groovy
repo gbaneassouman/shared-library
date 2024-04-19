ac9a5229b753f1977bdef4476302f1a84afd9a02
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-19 21:19:38 +0000

    update deploy.goovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -23,4 +23,18 @@
 def call(dirname) {
     sh "cat src/terraform/${dirname}/files/infos_ec2.txt"
     sh "export instance_ip=\$(cat src/terraform/${dirname}/files/infos_ec2.txt)"
+    sh "mkdir -p app-dir"
+    sh """
+    for LINE in \$(cat /var/lib/jenkins/workspace/projet-fil-rouge/list.txt)
+    do
+        cp -r src/"\$LINE" app-dir/
+    done
+    """
+    sh "cp src/scripts/deploy-apps.sh app-dir/ && cp src/terraform/${dirname}/files/infos_ec2.txt app-dir/"
+    sh "zip -r app-dir.zip app-dir/"
+    sh "scp -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no -r app-dir.zip $username@$instance_ip:~/"
+    sh "ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip 'unzip ~/app-dir.zip'"
+    sh "ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip 'chmod +x ~/app-dir/deploy-apps.sh'"
+    sh "ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$instance_ip 'cd ~/app-dir && sh deploy-apps.sh'"
+    sh "rm -rf ~/app-*"
 }
