1e09bd03887d728ec1d3e1bb8c739f4c756563f0
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-19 10:46:53 +0000

    refactore deployment code

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,48 +1,22 @@
 #!/usr/bin/env groovy
 /* groovylint-disable LineLength, NglParseError */
-/* groovylint-disable-next-line NglParseError */
-def exportIp(dirname) {
-    sh "export instance_ip=\$(cat src/terraform/${dirname}/files/infos_ec2.txt)"
-}
-//INFILE=\$PWD/list.txt
-def appDirname(dirname) {
+/* groovylint-disable-next-line MethodParameterTypeRequired, MethodReturnTypeRequired, NglParseError, NoDef */
+def call(dirname) {
     sh """
         #!/bin/bash
+        export instance_ip=\$(cat src/terraform/${dirname}/files/infos_ec2.txt)
         mkdir -p app-dir
         for LINE in \$(cat /var/lib/jenkins/workspace/projet-fil-rouge/list.txt)
         do
             cp -r src/"\$LINE" app-dir/
         done
-    """
-}
-
-def copyFile(dirname) {
-    sh """
-        #!/bin/bash
-        export instance_ip=\$(cat src/terraform/${dirname}/files/infos_ec2.txt)
         cp src/scripts/deploy-apps.sh app-dir/ && cp src/terraform/${dirname}/files/infos_ec2.txt app-dir/
         zip -r app-dir.zip app-dir/
         scp -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no -r app-dir.zip $username@${instance_ip}:~/
-    """
-}
-
-def unzipDir(dirname) {
-    sh """
-        export instance_ip=\$(cat src/terraform/${dirname}/files/infos_ec2.txt)
-        ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@${instance_ip} 'unzip ~/app-dir.zip' 
-        """
-}
-
-def apps(dirname) {
-    sh """
-        #!/bin/bash
-        export instance_ip=\$(cat src/terraform/${dirname}/files/infos_ec2.txt)
+        ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@${instance_ip} 'unzip ~/app-dir.zip'
         ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@${instance_ip} 'unzip ~/app-dir.zip'
         ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@${instance_ip} 'chmod +x ~/app-dir/deploy-apps.sh'
         ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@${instance_ip} 'cd ~/app-dir && sh deploy-apps.sh'
+        rm -rf ~/app-*
     """
-}
-
-def deleteDirs(dirname) {
-    sh "rm -rf ~/app-*"
 }
