503689c12038959561f33411c8b2d023ebf2d9d6
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-20 22:26:00 +0000

    add condition into deploy script

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,7 +2,7 @@
 /* groovylint-disable GStringExpressionWithinString, LineLength, NglParseError, NoDef */
 /* groovylint-disable-next-line MethodParameterTypeRequired, MethodReturnTypeRequired, NglParseError, NoDef */
 def call(String dirname) {
-    if ( ${dirname} == "staging" ) {
+    if ( dirname == "staging" ) {
         sh '''
             #!/bin/bash
             echo staging
