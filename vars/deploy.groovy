323e96d0da38554ba6fcecab2efab02fb78d5164
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-20 20:13:46 +0000

    send app-dir by scp to INSTANCE

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -4,7 +4,6 @@
 def call(dirname) {
     sh '''
         #!/bin/bash
-        echo jenkins.read-me.site > src/terraform/${dirname}/files/infos_ec2.txt
         export INSTANCE=$(cat src/terraform/${dirname}/files/infos_ec2.txt)
         mkdir -p app-dir
         for LINE in $(cat /var/lib/jenkins/workspace/projet-fil-rouge/list.txt)
@@ -20,29 +19,5 @@ def call(dirname) {
         sleep 5
         rm -rf ~/app-*
     '''
-}           //zip -r app-dir.zip app-dir/
-            //scp -i $TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no -r app-dir.zip $username@$INSTANCE:~/
-            //ssh -i $TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$INSTANCE 'unzip ~/app-dir.zip'
-            //ssh -i $TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$INSTANCE 'chmod +x ~/app-dir/deploy-apps.sh'
-            //ssh -i $TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$INSTANCE 'cd ~/app-dir && sh deploy-apps.sh'
-            
-/* groovylint-disable-next-line MethodParameterTypeRequired, MethodReturnTypeRequired */
-// def call(dirname) {
-//     sh 'export INSTANCE=\$(cat src/terraform/${dirname}/files/infos_ec2.txt)'
-//     def env = System.getenv()
-//     sh 'echo env['INSTANCE']'
-//     sh 'mkdir -p app-dir'
-//     sh """
-//     for LINE in \$(cat /var/lib/jenkins/workspace/projet-fil-rouge/list.txt)
-//     do
-//         cp -r src/"\$LINE" app-dir/
-//     done
-//     """
-    //sh 'cp src/scripts/deploy-apps.sh app-dir/ && cp src/terraform/${dirname}/files/infos_ec2.txt app-dir/'
-    //sh 'zip -r app-dir.zip app-dir/'
-    //sh "scp -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no -r app-dir.zip $username@$INSTANCE:~/"
-    //sh "ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$INSTANCE 'unzip ~/app-dir.zip'"
-    //sh "ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$INSTANCE 'chmod +x ~/app-dir/deploy-apps.sh'"
-    //sh "ssh -i \$TF_DIR/${dirname}/files/\$AWS_KEY_NAME.pem -o StrictHostKeyChecking=no  $username@$INSTANCE 'cd ~/app-dir && sh deploy-apps.sh'"
-    //sh 'rm -rf ~/app-*'
-//}
+}           
+
