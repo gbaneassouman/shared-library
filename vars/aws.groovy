554fc1a373a3d390ee26b888069571473a7e523e
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-21 12:55:10 +0000

    Add condition in deploy.groovy

--- vars/aws.groovy
+++ vars/aws.groovy
@@ -5,17 +5,36 @@
 /* groovylint-disable-next-line NoDef */
 def call(name) {
     /* groovylint-disable-next-line GStringExpressionWithinString */
-    sh '''
-        rm -rf devops-gbane.pem ~/.aws
-        mkdir -p ~/.aws
-        echo "[default]" > ~/.aws/credentials
-        echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
-        echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
-        echo "aws_profile=$AWS_PROFILE" >> ~/.aws/credentials
-        rm -rf $TF_DIR/${name}/files/$AWS_KEY_NAME.pem
-        cat "$AWS_PRIVATE_KEY" > $TF_DIR/${name}/files/$AWS_KEY_NAME.pem
-        chmod 400 $TF_DIR/${name}/files/$AWS_KEY_NAME.pem
-        chmod 400 ~/.aws/credentials
-    '''
+    if ( dirname == 'staging' ) {
+        sh '''
+            #!/bin/bashs
+            rm -rf $AWS_KEY_NAME.pem ~/.aws
+            mkdir -p ~/.aws
+            echo "[default]" > ~/.aws/credentials
+            echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
+            echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+            echo "aws_profile=$AWS_PROFILE" >> ~/.aws/credentials
+            rm -rf $TF_DIR/staging/files/$AWS_KEY_NAME.pem
+            cat "$AWS_PRIVATE_KEY" > $TF_DIR/staging/files/$AWS_KEY_NAME.pem
+            chmod 400 $TF_DIR/staging/files/$AWS_KEY_NAME.pem
+            chmod 400 ~/.aws/credentials
+        '''
+    }
+    else if ( dirname == 'prod' ) {
+        sh '''
+            #!/bin/bashs
+            rm -rf $AWS_KEY_NAME.pem ~/.aws
+            mkdir -p ~/.aws
+            echo "[default]" > ~/.aws/credentials
+            echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
+            echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+            echo "aws_profile=$AWS_PROFILE" >> ~/.aws/credentials
+            rm -rf $TF_DIR/prod/files/$AWS_KEY_NAME.pem
+            cat "$AWS_PRIVATE_KEY" > $TF_DIR/prod/files/$AWS_KEY_NAME.pem
+            chmod 400 $TF_DIR/prod/files/$AWS_KEY_NAME.pem
+            chmod 400 ~/.aws/credentials
+
+        '''
+    }
 }
 
