857f3912353df1270e3feb1b8183d2c30ebd7d95
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-30 19:20:09 +0000

    copy generate file by terraform

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -5,15 +5,19 @@ def install_kubernetes(String dirname) {
     if ( dirname == 'staging' ) {
         sh '''
             #!/bin/bash
-            
-            sleep 10
+            cp $TF_DIR/staging/staging.yml src/ansible/host_vars/staging.yml
+            cp $TF_DIR/staging/ic-webapp-deployment.yml src/ansible/playbooks/manifests/ic-webapp/ic-webapp-deployment.yml
+            cd $PWD/src/ansible
+            ansible-playbook -i hosts.yml playbooks/deploy-apps-staging.yml
         '''
     }
     else if ( dirname == 'prod' ) {
         sh '''
             #!/bin/bash
-            
-            sleep 10
+            cp $TF_DIR/prod/ic-webapp-deployment.yml src/ansible/playbooks/manifests/ic-webapp/ic-webapp-deployment.yml
+            cp $TF_DIR/prod/prod.yml src/ansible/host_vars/prod.yml
+            cd $PWD/src/ansible
+            ansible-playbook -i hosts.yml playbooks/deploy-apps-prod.yml
         '''
     }
 }
