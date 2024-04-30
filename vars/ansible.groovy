678c7afdd323071e3466895b0f86a4a1623a7cc4
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-30 19:33:26 +0000

    replace deploy-app by k3s.yml

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -8,7 +8,7 @@ def install_kubernetes(String dirname) {
             cp $TF_DIR/staging/staging.yml src/ansible/host_vars/staging.yml
             cp $TF_DIR/staging/ic-webapp-deployment.yml src/ansible/playbooks/manifests/ic-webapp/ic-webapp-deployment.yml
             cd $PWD/src/ansible
-            ansible-playbook -i hosts.yml playbooks/deploy-apps-staging.yml
+            ansible-playbook -i hosts.yml playbooks/staging-k3s.yml
         '''
     }
     else if ( dirname == 'prod' ) {
@@ -17,7 +17,7 @@ def install_kubernetes(String dirname) {
             cp $TF_DIR/prod/ic-webapp-deployment.yml src/ansible/playbooks/manifests/ic-webapp/ic-webapp-deployment.yml
             cp $TF_DIR/prod/prod.yml src/ansible/host_vars/prod.yml
             cd $PWD/src/ansible
-            ansible-playbook -i hosts.yml playbooks/deploy-apps-prod.yml
+            ansible-playbook -i hosts.yml playbooks/prod-k3s.yml
         '''
     }
 }
