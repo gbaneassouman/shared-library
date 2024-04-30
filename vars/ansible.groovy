2a331004aa89a1d1110727c17ac0ec73adce42f0
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-30 19:14:21 +0000

    copy generate file by terraform

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -5,14 +5,14 @@ def install_kubernetes(String dirname) {
     if ( dirname == 'staging' ) {
         sh '''
             #!/bin/bash
-            ls -l $TF_DIR/staging
+            
             sleep 10
         '''
     }
     else if ( dirname == 'prod' ) {
         sh '''
             #!/bin/bash
-            ls -l $TF_DIR/prod
+            
             sleep 10
         '''
     }
@@ -24,23 +24,20 @@ def deploy_apps(String dirname) {
     if ( dirname == 'staging') {
         sh '''
             #!/bin/bash
-            ls -l $TF_DIR/staging/
+            cp $TF_DIR/staging/staging.yml src/ansible/host_vars/staging.yml
+            cp $TF_DIR/staging/ic-webapp-deployment.yml src/ansible/playbooks/manifests/ic-webapp/ic-webapp-deployment.yml
+            cd $PWD/src/ansible
+            ansible-playbook -i hosts.yml playbooks/deploy-apps-staging.yml
         '''
     }
     /* groovylint-disable-next-line DuplicateStringLiteral */
     else if ( dirname == 'prod') {
         sh '''
             #!/bin/bash
-            ls -l $TF_DIR/prod/
+            cp $TF_DIR/prod/ic-webapp-deployment.yml src/ansible/playbooks/manifests/ic-webapp/ic-webapp-deployment.yml
+            cp $TF_DIR/prod/prod.yml src/ansible/host_vars/prod.yml
+            cd $PWD/src/ansible
+            ansible-playbook -i hosts.yml playbooks/deploy-apps-prod.yml
         '''
     }
 }
-
-//cp $TF_DIR/staging/staging.yml src/ansible/host_vars/staging.yml
-//cd $PWD/src/ansible
-//ansible-playbook -i hosts.yml playbooks/deploy-apps-staging.yml
-//cp $TF_DIR/prod/ic-webapp-deployment.yml src/ansible/playbooks/manifests/ic-webapp/ic-webapp-deployment.yml
-//cp $TF_DIR/prod/prod.yml src/ansible/host_vars/prod.yml
-//cd $PWD/src/ansible
-//ansible-playbook -i hosts.yml playbooks/deploy-apps-prod.yml
-//src/ansible/playbooks/manifests/ic-webapp/ic-webapp-deployment.yml
