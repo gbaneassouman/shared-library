05589eb53a120f6e5aec53b9cffebe68520086c5
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-30 18:31:22 +0000

    check the presence of generate file by terraform

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -5,20 +5,14 @@ def install_kubernetes(String dirname) {
     if ( dirname == 'staging' ) {
         sh '''
             #!/bin/bash
-            cp $TF_DIR/staging/ic-webapp-deployment.yml src/ansible/playbooks/manifests/ic-webapp/ic-webapp-deployment.yml
-            cp $TF_DIR/staging/staging.yml src/ansible/host_vars/staging.yml
-            cd $PWD/src/ansible
-            ansible-playbook -i hosts.yml playbooks/staging-k3s.yml
+            tree $TF_DIR/staging
             sleep 10
         '''
     }
     else if ( dirname == 'prod' ) {
         sh '''
             #!/bin/bash
-            cp $TF_DIR/prod/ic-webapp-deployment.yml src/ansible/playbooks/manifests/ic-webapp/ic-webapp-deployment.yml
-            cp $TF_DIR/prod/prod.yml src/ansible/host_vars/prod.yml
-            cd $PWD/src/ansible
-            ansible-playbook -i hosts.yml playbooks/prod-k3s.yml
+            tree $TF_DIR/prod
             sleep 10
         '''
     }
